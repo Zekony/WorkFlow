7c3aeec57c409a3980ac64f4053488eb7df82f8d
Zekony <Zhe6995@mail.ru>
2024-06-28 09:32:14 +0300

    added new recourses

--- resources/build.gradle.kts
+++ resources/build.gradle.kts
@@ -1,8 +1,15 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+import java.io.FileInputStream
+
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.com.android.library)
     alias(libs.plugins.org.jetbrains.kotlin.android)
 }
+
+private val keystorePropertiesFile = file("keys.properties")
+private val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 android {
     namespace = "com.finto.resources"
@@ -13,6 +20,8 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
+
+        buildConfigField("String", "SERVER_CLIENT_ID", keystoreProperties.getProperty("serverClientId"))
     }
 
     buildTypes {
@@ -32,6 +41,7 @@ android {
         jvmTarget = "17"
     }
     buildFeatures {
+        buildConfig = true
         compose = true
     }
     composeOptions {
